09ef424e0baa2d3ec39b3ee8fceb30294bff7859
Vinas1191 <vin987sav@gmail.com>
2025-03-21 10:08:27 -0300

    Resolved merge conflicts and applied local changes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,40 +22,40 @@ export default async function Home({ searchParams }: PropsType) {
 
   return (
     <>
-      <Suspense fallback={<OverviewCardsSkeleton />}>
+      {/* <Suspense fallback={<OverviewCardsSkeleton />}>
         <OverviewCardsGroup />
-      </Suspense>
+      </Suspense> */}
 
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
+      <div className="mt-4 grid grid-cols-6 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
         <PaymentsOverview
           className="col-span-12 xl:col-span-7"
           key={extractTimeFrame("payments_overview")}
           timeFrame={extractTimeFrame("payments_overview")?.split(":")[1]}
-        />
-
+      />
+{/* 
         <WeeksProfit
           key={extractTimeFrame("weeks_profit")}
           timeFrame={extractTimeFrame("weeks_profit")?.split(":")[1]}
           className="col-span-12 xl:col-span-5"
-        />
+        /> */}
 
-        <UsedDevices
+        {/* <UsedDevices
           className="col-span-12 xl:col-span-5"
           key={extractTimeFrame("used_devices")}
           timeFrame={extractTimeFrame("used_devices")?.split(":")[1]}
-        />
+        /> */}
 
-        <RegionLabels />
-
+        {/* <RegionLabels /> */}
+{/* 
         <div className="col-span-12 grid xl:col-span-8">
           <Suspense fallback={<TopChannelsSkeleton />}>
             <TopChannels />
           </Suspense>
-        </div>
+        </div> */}
 
-        <Suspense fallback={null}>
+        {/* <Suspense fallback={null}>
           <ChatsCard />
-        </Suspense>
+        </Suspense> */}
       </div>
     </>
   );
